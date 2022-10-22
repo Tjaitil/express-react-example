25e630922bde4fb73b6ce6f2632d8f1509305171
Tjaitil <52608380+Tjaitil@users.noreply.github.com>
2022-10-22 15:38:32 +0200

    Updated app.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,31 +1,15 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
 import "./App.css";
 import "./scss/app.scss";
+import PlayerDynamicTable from "./components/PlayerDynamicTable";
 
 function App() {
-    const [count, setCount] = useState(0);
-
-    return (
-        <div className="App">
-            <div>
-                <a href="https://vitejs.dev" target="_blank">
-                    <img src="/vite.svg" className="logo" alt="Vite logo" />
-                </a>
-                <a href="https://reactjs.org" target="_blank">
-                    <img src={reactLogo} className="logo react" alt="React logo" />
-                </a>
-            </div>
-            <h1>Vite + React</h1>
-            <div className="card">
-                <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
-                <p>
-                    Edit <code>src/App.tsx</code> and save to test HMR
-                </p>
-            </div>
-            <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
-        </div>
-    );
+  return (
+    <div className="App container-fluid">
+      <h1>Welcome!</h1>
+      <h2>Players from the League of Legends 2022 World Championship</h2>
+      <PlayerDynamicTable />
+    </div>
+  );
 }
 
 export default App;
